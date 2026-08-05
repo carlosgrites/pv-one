485cf89645188b3a9fa1359abf345983dfa2c566
carlosgrites <carlos.kartweb@gmail.com>
2026-08-05 15:53:29 -0300

    Update wix.js

--- api/wix.js
+++ api/wix.js
@@ -17,18 +17,16 @@ export default async function handler(req, res) {
 
   const WIX_API_KEY = process.env.WIX_API_KEY;
   const WIX_SITE_ID = "50bca98c-31f2-4172-a19d-c3abf3dd9dd7";
-  const WIX_ACCOUNT_ID = "74713a6e-e007-4df9-8cc4-a1058c55d05d";
 
   try {
-    const { title, content, coverImage } = req.body;
+    const { title, content } = req.body;
 
     const response = await fetch('https://www.wixapis.com/wix-data/v1/items', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': WIX_API_KEY,
-        'wix-site-id': WIX_SITE_ID,
-        'wix-account-id': WIX_ACCOUNT_ID
+        'wix-site-id': WIX_SITE_ID
       },
       body: JSON.stringify({
         dataCollectionId: "blog/posts",
